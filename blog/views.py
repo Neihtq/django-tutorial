214a506ba7d132a3e07a8737cba6adee25bb1349
neihtfool <q.thien.nguyen@outlook.de>
2020-10-21 21:30:47 +0200

    added data model

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': "TQN",
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 19, 2020'
-    },
-    {
-        'author': "John Doe",
-        'title': 'Blog Post 1',
-        'content': 'Second post content',
-        'date_posted': 'October 20, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
