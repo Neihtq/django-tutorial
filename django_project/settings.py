bebdc147ea60974e19583ae89e4498dd751bac42
neihtfool <q.thien.nguyen@outlook.de>
2020-10-26 16:37:55 +0100

    create register page

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
